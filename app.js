0a1d0b52a3835b9675924d34b7b7d0ff6c5da7db
Brede Børhaug <brede@borhaug.io>
2020-12-17 22:40:42 +0100

    Added refresh token function

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const _ = require('lodash')
 const clientId = '81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384'
 const clientSecret = 'c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3'
 
+// Set the refresh interval for the access token.
+const refreshInterval = 60*60*24*7 // 7 days in ms
 
 class MyApp extends Homey.App {
   /**
@@ -23,19 +25,22 @@ class MyApp extends Homey.App {
 
     //this._username = this.homey.settings.get('username')
     //this._password = this.homey.settings.get('password')
-    
+
     // Authenticate the user 
     this.authenticate()
-    
-    
+
+
+    // Refresh token every 7 days
+    this.refreshToken()
 
     // Setup the settings callback
     this.setupSettings()
 
     
-    
+
+
   }
-  
+
   async authenticate() {
     this._connected = false
     this._auth = null
@@ -49,24 +54,36 @@ class MyApp extends Homey.App {
       this._connected = true
       this._auth = token
       this.log('Authorization completed successfully')
+
     } catch (error) {
       this.log('The auth login process did not succeed. Change the settings')
     }
   }
 
-  isConnected(){
+  async refreshToken() {
+    setInterval( async () => {
+      const token = await Tesla.oauth.refresh({
+        refreshToken: this._auth.refreshToken,
+        clientId: clientId,
+        clientSecret: clientSecret
+      })
+      this._auth = token
+    }, refreshInterval) // call at interval according to refreshInterval variable
+  }
+
+  isConnected() {
     return this.isConnected
   }
 
-  getToken(){
-    if (this._connected == false){
+  getToken() {
+    if (this._connected == false) {
       return false
-    }else{
+    } else {
       return this._auth
     }
   }
 
-  async setupSettings(){
+  async setupSettings() {
     await this.homey.settings.on('set', async (args) => {
       //this.log('Settings updated. Call Authorize to get new token and connect to Tesla again.')
       await this.authenticate()
